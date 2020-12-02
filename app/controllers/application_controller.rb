f691edaebe9887fa2b1b8e148fd166bb6bab6078
erikn775 <erikn775@gmail.com>
2020-12-02 06:57:59 -0700

    commit tree 4f130a46706bfcd5063469fde030daaebe366600

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do 
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    erb :edit
+  end
+
+  get '/recipe/new' do
+    recipe = Recipe.create(params)
+  end
 
 end
